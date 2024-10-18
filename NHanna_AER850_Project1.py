0253e0b9c12bd00b5f169cedf4ba45478a94e864
Nathanael Hanna <128150666+Nathanael-e-h@users.noreply.github.com>
2024-10-18 10:23:22 -0400

    Oct17 Work

--- NHanna_AER850_Project1.py
+++ NHanna_AER850_Project1.py
@@ -9,19 +9,48 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+###############################
+# STEP 1: Data Processing
+###############################
 df = pd.read_csv("Project_1_Data.csv")
 print(df.info())
 
+
+###############################
+# STEP 2: Data Visualization
+###############################
+# do a 3d plot here
+
+# color code points based on step 1-13
+
+X_min, X_max = df['X'].min(), df['X'].max()
+Y_min, Y_max = df['Y'].min(), df['Y'].max()
+Z_min, Z_max = df['Z'].min(), df['Z'].max()
+
+DataVivPicture = plt.figure()
+ax = plt.axes(projection ='3d')
+
+# Plot the points
+ax.scatter(df['X'], df['Y'], df['Y'])
+
+# Set axis limits
+ax.set_xlim(X_min, X_max)
+ax.set_ylim(Y_min, Y_max)
+ax.set_zlim(Z_min, Z_max)
+
+
+###############################
+# STEP 3: 
+###############################
+# is the correlation graph
+
+
+###############################
+# STEP 4: 
+###############################
+# 3 with grid search, one with randomized
 """Data Splitting Hell Yeah"""
-#Stratified Sampling (strongly recommended by the professor)
-df["income_categories"] = pd.cut(df["median_income"],
-                          bins=[0, 2, 4, 6, np.inf],
-                          labels=[1, 2, 3, 4])
-my_splitter = StratifiedShuffleSplit(n_splits = 1,
-                               test_size = 0.2,
-                               random_state = 42)
-for train_index, test_index in my_splitter.split(df, df["income_categories"]):
-    strat_df_train = df.loc[train_index].reset_index(drop=True)
-    strat_df_test = df.loc[test_index].reset_index(drop=True)
-strat_df_train = strat_df_train.drop(columns=["income_categories"], axis = 1)
-strat_df_test = strat_df_test.drop(columns=["income_categories"], axis = 1)
+# Stratified Sampling (strongly recommended by the professor)
+
+
+# REPORT: intro, step step step, plots, explanation, conclusion w/ github URL
